34819c2ad1ee23b883db062fd1bca3dcda9daffc
DuyNLK <nguyenlukhanhduy@gmail.com>
2023-12-13 23:21:35 +0700

    convert indent from space to tab

--- script/end.js
+++ script/end.js
@@ -1,14 +1,13 @@
-	
 	if(sessionStorage.record==null)
 	{
-		score=0;	
+		score=0;
 	}else{
-		score=sessionStorage.record;		
+		score=sessionStorage.record;
 	}
-	
+
 	window.onload=function()
 					{
 						document.getElementById("score").innerHTML=score;	
 					}
-	
+
 	setTimeout(function(){window.location.assign("index.html");},5000);
